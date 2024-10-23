be0bf7ac7d8ba4b6926616e3daa3a7b31aa401e9
Logan Kirkland <logan@logankirk.land>
2024-10-23 16:42:57 -0400

    Create environment only in docker build, then run tests in container

--- src/term_assist/test/run.sh
+++ src/term_assist/test/run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit
-docker build --build-arg now="$(date +%Y-%m-%d_%H-%M-%S)" --tag "pytest-runner" ../../..
-docker create --name dummy "pytest-runner"
-docker cp dummy:app/src/term_assist/test/output/. output/
-docker rm -f dummy
-docker image rm -f pytest-runner
+docker build --tag "pytest-runner-image" ../../..
+now=(date +%Y-%m-%d_%H-%M-%S)
+docker run --name pytest-runner-container --env-file ../../../.env \
+  pytest-runner-image /bin/bash -c \
+  "mkdir output; python3 -m pytest test.py \
+  --junit-xml=output/${now}_output.xml | tee output/${now}_output.log"
+docker cp pytest-runner-container:app/src/term_assist/test/output/. output/
+docker rm -f pytest-runner-container
+docker image rm -f pytest-runner-image
 cd - || exit
